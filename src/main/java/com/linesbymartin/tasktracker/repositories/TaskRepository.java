6dfa93a13b429592aae1407f4a01dd86ad99016c
linesbymartin <contact@linesbymartin.com>
2025-08-25 23:33:55 +0300

    feat: created task repository

---
+++ src/main/java/com/linesbymartin/tasktracker/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.linesbymartin.tasktracker.repositories;
+
+import com.linesbymartin.tasktracker.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+interface TaskRepository extends JpaRepository<Task, UUID> {
+}
