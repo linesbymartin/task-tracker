242aad843fdb1d7703a91a71ceffaba4b7ffed93
linesbymartin <contact@linesbymartin.com>
2025-08-26 08:59:28 +0300

    feat: expanded task repository

--- src/main/java/com/linesbymartin/tasktracker/repositories/TaskRepository.java
+++ src/main/java/com/linesbymartin/tasktracker/repositories/TaskRepository.java
@@ -4,8 +4,14 @@ import com.linesbymartin.tasktracker.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-interface TaskRepository extends JpaRepository<Task, UUID> {
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findAllByTaskListId(UUID taskListId);
+
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
 }
